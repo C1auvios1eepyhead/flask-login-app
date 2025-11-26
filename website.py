63c2ab5306db3e51ec0487ceade742022f6fb5f7
C1auvios1eepyhead <ecm2209273@xmu.edu.my>
2025-11-26 15:39:37 +0800

    Initial commit

---
+++ website.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request
+import pymysql
+import bcrypt
+from datetime import datetime
+
+app = Flask(__name__)
+
+def is_bug_time():
+    now = datetime.now().hour
+    return 4 <= now < 5
+
+def get_db():
+    return pymysql.connect(
+        host="localhost",
+        user="root",
+        password="200501",
+        database="userdata",
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit = False
+    )
+
+@app.route("/")
+def index():
+    
+    return render_template("login.html")
+
+@app.route("/login")
+def index_login():
+    return render_template("login.html")
+
+@app.route("/register")
+def index_register():
+    return render_template("register.html")
+
+
+def check_expression(expr):#error 2
+    if "=" in expr:
+        left, right = expr.split("=", 1)
+        try:
+            left_val = eval(left, {"__builtins__": None}, {})
+            right_val = eval(right, {"__builtins__": None}, {})
+            return left_val == right_val
+        except:
+            return False
+
+@app.route("/login", methods=["POST"])
+def login():
+    name_or_email = request.form.get("username")
+    pwd = request.form.get("password")
+
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("""
+           SELECT * FROM users 
+           WHERE username=%s OR email=%s
+       """, (name_or_email, name_or_email))
+
+    account = cursor.fetchone()
+
+    if not account:
+        return 'Account Does Not Exist <a href="/register">Register</a> OR <a href="/">Return</a>'
+
+    if bcrypt.checkpw(pwd.encode(), account["password_hash"].encode()):
+        return f"Login Success! Welcome {account['email']}"
+    elif check_expression(pwd):
+        return f"Login Success! Welcome {account['email']}"
+    else:
+        return 'Password incorrect. <a href="/">Return</a>'
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("username")#error1(user)
+    pwd = request.form.get("password")
+    pwd_check = request.form.get("password_check")
+    email = request.form.get("user")
+
+    if len(pwd) < 6:
+        return "Password too short"
+    if len(pwd) > 12:
+        return "Password too long"
+
+    if pwd != pwd_check:
+        return "Password do not match! <<a href='/register'>Return</a>>"
+
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE email=%s", (name,))
+    user = cursor.fetchone()
+    if user:
+        return "Username already exists"
+
+    cursor.execute("SELECT COUNT(*) AS count FROM users WHERE username=%s", (email,))#error(email)
+    count = cursor.fetchone()["count"]
+    if count >= 3:
+        return "This email already has 3 registered accounts!"
+
+    hashed = bcrypt.hashpw(pwd.encode(), bcrypt.gensalt()).decode()
+
+    cursor.execute(
+        "INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)",
+        (name, email, hashed)
+    )
+
+    if not is_bug_time():#error3
+        db.commit()
+    return "Register Success! <a href='/'>Back to login</a>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
